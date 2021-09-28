ee1c9f83ac740849267709a9ad2bd2606ce4d75d
Alexplus77 <alexplus77@mail.ru>
2021-09-28 18:13:53 +0200

    Сделал задачу1 по видео

--- task1/task.js
+++ task1/task.js
@@ -1,17 +1,43 @@
 //Задача 1
-let sum = 0
-const tickets = (arr) => {
-    const lastPay = arr.pop()
-    sum = arr.reduce((elem, acc) => {
-        return elem + acc
-    }, 0)
-    if (lastPay > sum) {
-        return 'No. Сдачи нет.'
-    } else {
-        return 'Yes. Сдача есть.'
-    }
+const bills={
+    25:0,
+    50:0,
+    100:0
 }
-console.log(tickets([25, 25, 50]))// Yes
-console.log(tickets([25, 100]))// No
-console.log(tickets([25, 25, 50, 50, 100]))// У меня "Yes".Почему у тебя "No"? Ведь он дает 100, а в кассе 150, 75 сдачи есть.
+const tickets = (arr) => {
+
+ for (let i=0; i<arr.length; i++) {
+     let bill=arr[i]
+
+      if (bill === 50) {
+         if (bills[25]) {
+             bills[25]--
+             bills[bill]++
+         } else {
+             return 'No'
+         }
+     }
+     console.log(bills)
+     if (bill === 100) {
+         if (bills[50] && bills[25]) {
+             bills[50]--
+             bills[25]--
+         } else if (bills[25] >= 3) {
+             bills[25] -= 3
+         } else {
+             return 'No'
+         }
+     }
+     bills[bill]++
+ }
+    return 'Yes'
+}
+
+
+
+
+
+//console.log(tickets([25, 25, 50]))// Yes
+//console.log(tickets([25, 100]))// No
+ console.log(tickets([25, 25, 50, 50, 100]))// У меня "Yes".Почему у тебя "No"? Ведь он дает 100, а в кассе 150, 75 сдачи есть.
 
