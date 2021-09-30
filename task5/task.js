db2fa868a4e5e1bc37c8efe7a6bf0dc7ef5affbd
Alexplus77 <alexplus77@mail.ru>
2021-09-30 13:27:20 +0200

    Сделал задачу 5, лепестки

---
+++ task5/task.js
@@ -0,0 +1,34 @@
+/**
+ * Кто вспоминает времена, когда они были на школьном дворе, когда девочки брали цветок и рвали его лепестки, произнося
+ * каждую из следующих фраз каждый раз, когда лепесток рвался:
+ ы
+ I love you
+ a little
+ a lot
+ passionately
+ madly
+ not at all
+ Когда был сорван последний лепесток, раздались крики возбуждения, мечты, нахлынувшие мысли и эмоции.
+ Ваша цель в этом ката - определить, какую фразу девушки сказали бы для цветка с заданным количеством лепестков,
+ где nb_petals> 0.
+
+ howMuchILoveYou(7),"I love you")
+ howMuchILoveYou(3),"a lot")
+ howMuchILoveYou(6),"not at all")
+ Числа могут быть разные и 1000 и 500, надо правильно расчитать, какая фраза выпадет на определенное число
+ **/
+
+const howMuchILoveYou = (petals) => {
+
+    const setWords = ['I love you', 'a little', 'a lot', 'passionately', 'madly', 'not at all']
+
+    while (petals > setWords.length) {
+        petals = petals - setWords.length
+    }
+    return setWords[petals - 1]
+}
+
+console.log(howMuchILoveYou(7))//"I love you"
+console.log(howMuchILoveYou(6))//"not at all"
+console.log(howMuchILoveYou(3))//"a lot"
+console.log(howMuchILoveYou(1000))//"passionately"
