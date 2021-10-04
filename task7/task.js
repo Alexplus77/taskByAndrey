13d82fae6ab028dfa8df3e95306d1a5730190ee5
Alexplus77 <alexplus77@mail.ru>
2021-10-04 19:33:45 +0200

    рекурсия

--- task7/task.js
+++ task7/task.js
@@ -7,13 +7,21 @@
  16 --> 1 + 6 = 7
  942 --> 9 + 4 + 2 = 15 --> 1 + 5 = 6
  132189 --> 1 + 3 + 2 + 1 + 8 + 9 = 24 --> 2 + 4 = 6
- 493193 --> 4 + 9 + 3 + 1 + 9 + 3 = 29 --> 2 + 9 = 11 --> 1 + 1 = 2**/
+ 493193 --> 4 + 9 + 3 + 1 + 9 + 3 = 29 --> 2 + 9 = 11 --> 1 + 1 = 2
+ **/
+
 
 function digital_root(n) {
- return n.toString().split('').reduce((acc, digit)=>{
-   return acc+Number(digit)
- }, 0)
+    const arrDigits = n.toString().split('')
 
-
+    if (arrDigits.length === 1) {
+        return n
+    } else {
+        n = arrDigits.reduce((acc, digit) => {
+            return acc + Number(digit)
+        }, 0)
+        return digital_root(n)
+    }
 }
-console.log(digital_root(16))
+
+console.log(digital_root(493193))
